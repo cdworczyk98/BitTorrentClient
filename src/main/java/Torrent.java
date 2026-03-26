14534a2baccff9d3c28a298c03056801594cda0c
Chris <cdworczyk@gmail.com>
2026-03-26 21:33:21 +0000

    torrent file progress

--- src/main/java/Torrent.java
+++ src/main/java/Torrent.java
@@ -1,6 +1,8 @@
 import lombok.Getter;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -11,7 +13,7 @@ public class Torrent {
     private String fileDirectory; //needs constructor
     private String downloadDirectory;
 
-    private List<Tracker> trackers;
+    private List<Tracker> trackers = new ArrayList<>();
     private String comment;
     private String createdBy;
     private LocalDateTime creationDate;
@@ -42,6 +44,56 @@ public class Torrent {
     private final byte[] infoHash = new byte[20];
     private  String hexStringInfoHash; //needs constructor
     private String urlSafeStringInfoHash; //needs constructor
+
+    //public event EventHandler<List<IPEndPoint>> PeerListUpdated;
+
+    private Object[] fileWriterLocks;
+
+    public Torrent(String name, String location, List<FileItem> files, List<String> trackers, int piecesSize, byte[] pieceHashes, int blockSize) throws IOException {
+        this.name = name;
+        this.downloadDirectory = location;
+        this.files = files;
+        fileWriterLocks = new Object[files.size()];
+        for (int i = 0; i < this.files.size(); i++) {
+            this.fileWriterLocks[i] = new Object();
+        }
+
+        if (trackers != null) {
+            for (String url : trackers) {
+                Tracker tracker = new Tracker(url);
+                this.trackers.add(tracker);
+                //tracker.PeerListUpdated += HandlePeerListUpdated;
+            }
+        }
+
+        this.piecesSize = piecesSize;
+        this.blockSize = blockSize;
+
+        int count = (int) Math.ceil(this.totalSize / (double) this.piecesSize);
+
+        this.pieceHashes = new byte[count][];
+        this.isPieceVerified = new boolean[count];
+        this.isBlockAcquired = new boolean[count][];
+
+        for (int i = 0; i < this.pieceCount; i++) {
+            this.isBlockAcquired[i] = new boolean[getBlockCount(i)];
+        }
+
+        if (pieceHashes != null) {
+            for (int i = 0; i < this.pieceCount; i++) {
+                this.pieceHashes[i] = getHash(i);
+            }
+        } else {
+            for (int i = 0; i < this.pieceCount; i++) {
+                this.pieceHashes[i] = new byte[20];
+                //Buffer.BlockCopy(pieceHashes, i * 20, PieceHashes[i], 0, 20);
+            }
+        }
+
+        Object info = TorrentInfoToBEncodingObject(this);
+        byte[] bytes = BEncoding.Encode(info);
+        infoHash = DigestUtils.sha1Hex("my string")
+    }
 
     public int getPieceSize(int piece) {
         if (piece == pieceCount - 1) {
--- src/main/java/Tracker.java
+++ src/main/java/Tracker.java
@@ -1,14 +1,33 @@
 import lombok.Getter;
 
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiConsumer;
+
 @Getter
 public class Tracker {
 
-    //public event EventHandler<List<IPEndPoint>> PeerListUpdated;
+    private final List<BiConsumer<Object, List<InetSocketAddress>>> peerListUpdatedListeners = new ArrayList<>();
 
     private String address;
 
     public Tracker(String address) {
         this.address = address;
+    }
+
+    public void addPeerListUpdatedListener(BiConsumer<Object, List<InetSocketAddress>> listener) {
+        peerListUpdatedListeners.add(listener);
+    }
+
+    public void removePeerListUpdatedListener(BiConsumer<Object, List<InetSocketAddress>> listener) {
+        peerListUpdatedListeners.remove(listener);
+    }
+
+    protected void onPeerListUpdated(List<InetSocketAddress> peers) {
+        for (BiConsumer<Object, List<InetSocketAddress>> listener : peerListUpdatedListeners) {
+            listener.accept(this, peers);
+        }
     }
 
 }
